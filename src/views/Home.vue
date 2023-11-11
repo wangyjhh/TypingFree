f67019772547b6e31a1b5ac6a00addea2cd1fc8e
hikaru <827394320@qq.com>
2023-11-12 00:53:41 +0800

    🐳 chore: commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div w="100%" h="100%" p-20px flex flex-col justify-center flex-items-center>
-        <div w="100%" text-center font-size-60px m-b="100px" flex flex-items-baseline justify-center>
-            <div>{{ str }}</div>
-            <div w-15px h-22px border="#d4d4d4d5" :class="isShow ? `border-b-5px` : ``"></div>
+        <div w="60%" border-rd="10px" text-center font-size-60px m-b="100px" flex flex-items-baseline justify-center
+            bg="#1b1b1b" p-50px>
+            <div id="typing"></div>
         </div>
         <div>
             <button btn m-r-50px @click="router.push('/Typing')">开始练习</button>
@@ -21,37 +21,40 @@
             </div>
         </template>
     </Dialog>
+    <span></span>
 </template>
+
 
 <script lang='ts' setup>
 import { ref, onMounted } from 'vue'
 import router from '@/router/index'
 import Dialog from '@/components/Dialog.vue'
-import { usText } from '@/composables/index';
+import { usText } from '@/composables/index'
+import TypeIt from "typeit";
 
-const isShow = ref(false)
+let cursor = ref(`
+<span style="margin-left:8px;background:rgba(249, 250, 252, 0.2);color:#f9faf;box-shadow:#ff7008 0px -4px 0px inset">|</span>`)
 
 onMounted(() => {
-    setInterval(() => {
-        isShow.value = !isShow.value
-    }, 500)
-    autoPrint()
+    // @ts-ignore
+    new TypeIt("#typing", {
+        startDelay: 1000,
+        deleteSpeed: 100,
+        loop: true,
+        loopDelay: 2000,
+        cursorChar: cursor.value
+    })
+        .type(`<span style="background: rgba(48, 178, 140, 0.2);color:#3aa675;">Typing </span>`)
+        .type(`<span style="background: rgba(229, 79, 74, 0.2);color:#e54f4a;">Bound</span>`)
+        .pause(1000)
+        .delete(5, { delay: 350 })
+        .type(`<span style="background: rgba(253, 194, 79, 0.2);color:#fdc24f;">Free</span>`)
+        .pause(2000)
+        .delete(11, { delay: 350 })
+        .type(`Typing Free`)
+        .pause(5000)
+        .go()
 })
-
-const title = 'Nice Typing'
-let str = ref("")
-
-const autoPrint = () => {
-    let index = 0
-    let Time = setInterval(() => {
-        if (index >= title.length - 1) {
-            clearInterval(Time)
-            return
-        }
-        str.value += title.substring(index, index + 1)
-        index++
-    }, 150)
-}
 
 usText.value = ""
 
